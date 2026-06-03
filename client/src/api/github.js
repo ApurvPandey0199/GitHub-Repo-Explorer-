1340dbd392f0cb72239d890ac06550d585f10f25
ApurvPandey0199 <apurvpandey0199@users.noreply.github.com>
2026-06-04 00:47:26 +0530

    chore: configure github pages deployment

--- client/src/api/github.js
+++ client/src/api/github.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD 
-    ? '/api/github' 
-    : (import.meta.env.VITE_API_URL || 'http://localhost:4000') + '/api/github'
+  baseURL: 'https://api.github.com'
 });
 
 const handleError = (error) => {
